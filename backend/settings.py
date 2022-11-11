3af9922417dea4454d6dea8cae9a1bc12f5aaca3
Eyasu Emana <61460804+EyasuEmana@users.noreply.github.com>
2022-11-11 15:51:07 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -202,8 +202,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    'http://127.0.0.1:3000',
-    'https://savelinkapplication.herokuapp.com/'
+    'http://127.0.0.1:3000'
 ]
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'frontend/build/static')
